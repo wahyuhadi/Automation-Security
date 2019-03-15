568a7de7c742b8312721435c37737d8d69af3c27
hrahmatwahyu <wahyu@gits.co.id>
2019-03-15 14:02:16 +0700

    add auto crawler

---
+++ check/check.go
@@ -0,0 +1,63 @@
+package check
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/gocolly/colly"
+)
+
+func IsCheck(isURL string) {
+	URL := isURL
+	if URL == "" {
+		log.Println("missing URL argument")
+		return
+	}
+
+	c := colly.NewCollector()
+
+	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		link := e.Attr("href")
+		if link != "" && link != "#" {
+			fmt.Println("[+] Is Parent ", link)
+			IsCheckParentURL(link)
+		}
+
+	})
+
+	c.OnError(func(r *colly.Response, err error) {
+		log.Println("error:", r.StatusCode, err)
+	})
+
+	c.Visit(URL)
+}
+
+func IsCheckParentURL(isURL string) {
+	URL := isURL
+	if URL == "" {
+		log.Println("missing URL argument")
+		return
+	}
+
+	c := colly.NewCollector()
+
+	c.OnHTML("form[action]", func(e *colly.HTMLElement) {
+		link := e.Attr("action")
+		if link != "" && link != "#" {
+			fmt.Println("[+] Is Child ", link)
+		}
+
+	})
+
+	// c.OnHTML("input[name]", func(e *colly.HTMLElement) {
+	// 	link := e.Attr("name")
+	// 	fmt.Println(link)
+
+	// })
+
+	c.OnError(func(r *colly.Response, err error) {
+		log.Println("error:", r.StatusCode, err)
+	})
+
+	c.Visit(URL)
+}

